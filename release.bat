9a23bb60573ad7dab1ce1b32f138858d6bc1a47c
Logan Arias <71857092+Logan-Arias-LPS@users.noreply.github.com>
2026-04-23 14:35:28 -0400

    renaming this as a pulgin named "LPS Student Contacts"

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir dists
-git archive --format zip --output dists\LPS-PL4WC-Custom.zip --worktree-attributes --verbose -9 HEAD:PL4WC .
-git archive --format zip --output dists\LPS-PL4WCExtensions-Custom.zip --worktree-attributes --verbose -9 HEAD:PL4WCExtensions .
+git archive --format zip --output dists\LPS-Contacts-Custom.zip --worktree-attributes --verbose -9 HEAD:LPS-Contacts .
+git archive --format zip --output dists\LPS-ContactsExtensions-Custom.zip --worktree-attributes --verbose -9 HEAD:LPS-ContactsExtensions .
 pause
